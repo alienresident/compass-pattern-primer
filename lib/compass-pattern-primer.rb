1c74e78c337179597859adc1fefc3aec2f436521
Mark Reilly <mark@alienresident.net>
2014-04-14 22:44:38 -0400

    lowered dependency

--- lib/compass-pattern-primer.rb
+++ lib/compass-pattern-primer.rb
@@ -18,7 +18,7 @@ Compass::Frameworks.register('compass-pattern-primer', :path => extension_path)
 #    a prerelease version
 #  Date is in the form of YYYY-MM-DD
 module Compass_Pattern_Primer
-  VERSION = "0.4.4"
+  VERSION = "0.4.5"
   DATE = "2014-04-14"
 end
 
