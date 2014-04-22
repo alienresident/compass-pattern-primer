5482c28dd6b3f4ebd6615d3a5f80b83c4ad1f044
Mark Reilly <mark@alienresident.net>
2014-04-21 22:54:09 -0400

    Merge branch 'master' into dev
    Conflicts:
    lib/compass-pattern-primer.rb
    templates/shared/sass/base/forms/_forms.scss

--- lib/compass-pattern-primer.rb
+++ lib/compass-pattern-primer.rb
@@ -18,8 +18,8 @@ Compass::Frameworks.register('compass-pattern-primer', :path => extension_path)
 #    a prerelease version
 #  Date is in the form of YYYY-MM-DD
 module Compass_Pattern_Primer
-  VERSION = "0.4.5"
-  DATE = "2014-04-14"
+  VERSION = "0.4.6"
+  DATE = "2014-04-21"
 end
 
 # This is where any custom SassScript should be placed. The functions will be  
